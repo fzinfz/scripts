61f88908f3b04d7617a4b492e3e56c1f467d9fcb
fzinfz <fzinfz@gmail.com>
2017-07-06 22:34:25 +0800

    Update bbr_enable.sh

--- bbr_enable.sh
+++ bbr_enable.sh
@@ -1,3 +1,8 @@
+# Manually Load and check BBR module(Optional)
+modprobe tcp_bbr
+lsmod | grep bbr
+sysctl net.ipv4.tcp_available_congestion_control
+
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
