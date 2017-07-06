b3aa5dd4f95e1ddd2ed2e69661c2798b6d9a1acb
fzinfz <fzinfz@gmail.com>
2017-07-07 03:04:19 +0800

    Update install-tools--apt-or-yum.sh

--- install-tools--apt-or-yum.sh
+++ install-tools--apt-or-yum.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-$1 update 
-
-if [ "$1" = "apt" ];then
-	apt install -y apt-utils
+if command -v apt >/dev/null 2>/dev/null; then
+	$c=apt
+elif command -v yum >/dev/null 2>/dev/null; then
+	$c=yum
+else
+	echo "only apt & yum supported"
+	exit
 fi
 
-if [ "$1" = "yum" ];then
+$c update 
+
+if [ "$c" = "apt" ];then
+	apt install -y apt-utils
+elif [ "$c" = "yum" ];then
 	yum install -y epel-release
 fi
 
-$1  install -y \
+$c  install -y \
     software-properties-common \
     net-tools bridge-utils dnsutils tcpdump  mtr nmap nethogs traceroute bmon iputils-ping nload iftop \
     trickle wondershaper \
