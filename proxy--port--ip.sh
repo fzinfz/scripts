837f3316a85b92ef9fd4ca202f357a485387d11f
Ferro Fang <fzinfz@gmail.com>
2017-07-09 15:06:07 +0800

    update

--- proxy--port--ip.sh
+++ proxy--port--ip.sh
@@ -1,16 +1,18 @@
-if [ -z ${2+x}; then
+if [ -z ${2+x} ]; then
   ip="127.0.0.1"
  else
   ip=$2
- fi
+fi
 
-if [ -z ${1+x}; then
+if [ -z ${1+x} ]; then
   port=1080
  else
   port=$1
- fi
- 
-export http_proxy=http://$ip:$port  
+fi
+
+http_proxy=http://$ip:$port
+
+export http_proxy=$http_proxy  
 export https_proxy=$http_proxy   
 export no_proxy="localhost,127.0.0.1,192.168.*.*,10.*.*.*,172.16.*.*,172.17.*.*"  
 export ftp_proxy=$http_proxy  
