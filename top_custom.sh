fa3736800c8f09d2a43a75a42fc01f4e3dfe95f3
Ferro Fang <fzinfz@gmail.com>
2017-07-17 14:56:39 +0800

    update

--- top_custom.sh
+++ top_custom.sh
@@ -2,5 +2,7 @@
 #watch -n 1  cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 watch --color -n 1 \
 	"inxi -C && printf '\n' && \
+	 top -b -o %CPU | head -n12 && printf '\n' && \
+	 top -b -o %MEM | head -n12 | tail -n6 && printf '\n' && \
 	 ps -eo %cpu,pid,command --sort -%cpu | head -n5 && printf '\n' && \
 	 ps -eo %mem,pid,command --sort -%mem | head -n5"
