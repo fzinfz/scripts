5d6d4fb0e15c9c7f8a5a9f5d30f280f1a581fdbf
Ferro Fang <fzinfz@gmail.com>
2017-07-22 04:22:20 +0800

    update

--- top_custom.sh
+++ top_custom.sh
@@ -1,6 +1,7 @@
 #dmidecode -t processor | grep Speed
 #watch -n 1  cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 watch --color -n 1 \
-	"inxi -C && printf '\n' && \
+	"top -b | head -n5 && printf '\n' && \
+	 inxi -C && printf '\n' && \
 	 ps -eo %cpu,pid,command --sort -%cpu | head -n5 && printf '\n' && \
 	 ps -eo %mem,pid,command --sort -%mem | head -n5"
