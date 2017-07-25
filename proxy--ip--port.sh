9bdd59badc0d65dc79ee096d825db2d9ba9bb364
Ferro Fang <fzinfz@gmail.com>
2017-07-26 01:56:15 +0800

    update

--- proxy--ip--port.sh
+++
@@ -1,5 +0,0 @@
-export http_proxy=http://$1:$2/
-export https_proxy=$http_proxy 
-export no_proxy="localhost,127.0.0.1,192.168.*.*,10.*.*.*,172.16.*.*,172.23.*.*"
-export ftp_proxy=$http_proxy
-export rsync_proxy=$http_proxy
