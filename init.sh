50908653537d90c16320ed15868a8d27fd9f3125
Ferro Fang <fzinfz@gmail.com>
2017-08-11 13:15:16 +0800

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
-alias git_add_commit_push="git add -A ; git commit -am 'update' ; git push"
 alias ls="ls --color=auto"
 
 shopt -s histappend
 PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
 
 export TZ=Asia/Shanghai
+
+# add current file path to $PATH
+CURRENT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
+if [[ ! $PATH = *"$CURRENT_DIR"* ]];then
+    export PATH=$PATH:$CURRENT_DIR
+fi
--- init_vimrc.sh
+++ init_vimrc.sh
@@ -1,4 +1,7 @@
 cat >> ~/.vimrc << EOL
 set mouse=c
 syntax on
+
+set modeline
+set tabstop=8 expandtab shiftwidth=4 softtabstop=4
 EOL
