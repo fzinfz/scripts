482d91fe43e2a456dfb060452e6f4f41c567f9bf
Ferro Fang <Ferro Fang>
2017-12-05 00:40:57 +0800

    update check_video

--- init.sh
+++ init.sh
@@ -283,6 +283,18 @@ check_video() {
     glxinfo | grep direct
     egrep -i " connected|card detect|primary dev|Setting driver" /var/log/Xorg.*.log
     egrep "EE" /var/log/Xorg.*.log
+
+    # https://wiki.archlinux.org/index.php/kernel_mode_setting#Forcing_modes_and_EDID
+    echo "Current status of connectors: "
+    for p in /sys/class/drm/*/status; \
+        do con=${p%/status}; echo -n "${con#*/card?-}: "; cat $p;
+    done
+}
+
+check_video--card_index(){
+    echo "card$1:"
+    cat /sys/class/drm/card$1/device/{label,uevent,power_method,power_dpm_state}
+    cat /sys/kernel/debug/dri/$1/radeon_{fence_info,gem_info,pm_info,sa_info,vram_mm}
 }
 
 bbr_check() {
