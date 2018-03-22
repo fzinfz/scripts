a65cc139acf6e39e1a9f62ab9cfe185b635105eb
Ferro Fang <Ferro Fang>
2018-03-22 15:34:46 +0800

    update

---
+++ vendor/Hyper-V_DDA.ps1
@@ -0,0 +1,24 @@
+﻿$vmName = 'Win10ent'
+$instanceId = '*VEN_1002&DEV_AA01*'
+$ErrorActionPreference = 'Stop'
+$vm = Get-VM -Name $vmName
+$dev = (Get-PnpDevice -PresentOnly).Where{ $_.InstanceId -like $instanceId }
+if (@($dev).Count -eq 1) {
+
+
+Disable-PnpDevice -InstanceId $dev.InstanceId -Confirm:$false
+$locationPath = (Get-PnpDeviceProperty -KeyName DEVPKEY_Device_LocationPaths -InstanceId $dev.InstanceId).Data[0]
+Dismount-VmHostAssignableDevice -LocationPath $locationPath -Force -Verbose
+Set-VM -VM $vm -DynamicMemory -MemoryMinimumBytes 3024MB -MemoryMaximumBytes 4096MB -MemoryStartupBytes 3024MB -AutomaticStopAction TurnOff
+
+# If you want to play with GPUs:
+Set-VM -VM $vm -StaticMemory -MemoryStartupBytes 3024MB -AutomaticStopAction TurnOff
+Set-VM -VM $vm -GuestControlledCacheTypes $true -LowMemoryMappedIoSpace 3024MB -HighMemoryMappedIoSpace 4096MB -Verbose
+
+Add-VMAssignableDevice -VM $vm -LocationPath $locationPath -Verbose
+
+} else {
+
+$dev | Sort-Object -Property Class | Format-Table -AutoSize
+Write-Error -Message ('Number of devices: {0}' -f @($dev).Count)
+}
---
+++ vendor/Hyper-V_DDA_re-enable.ps1
@@ -0,0 +1,9 @@
+﻿# Remove all devices from a single VM
+Remove-VMAssignableDevice -VMName win10ent -Verbose
+
+# Return all to host
+Get-VMHostAssignableDevice | Mount-VmHostAssignableDevice -Verbose
+
+# Enable it in devmgmt.msc
+(Get-PnpDevice -PresentOnly).Where{ $_.InstanceId -match 'VEN_1002&DEV_68A9' } |
+Enable-PnpDevice -Confirm:$false -Verbose
