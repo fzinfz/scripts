bfdc3265a49de9d4a2ba2841281b9df4b4c5c421
fzinfz <fzinfz@gmail.com>
2018-12-20 18:10:35 +0800

    update

--- linux/disk.sh
+++ linux/disk.sh
@@ -5,9 +5,9 @@ echo_info(){ echo -e "\033[92m$1\033[m"; }
 echo_info "lshw"
 lshw -class disk -short
 
-echo_info "lsblk"
+echo_info "lsblk -S"
 lsblk -S | sort
-echo
+echo_info "lsblk | grep"
 lsblk  | grep -vE 'loop|rom'
 
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/5/html/logical_volume_manager_administration/disk_remove_ex
@@ -18,9 +18,11 @@ pvs -o+pv_used
 echo_info "lvs"
 lvs
 
+grep_disk(){ grep -P "/(mapper|\wd\w\d?\b)"; }
+
 echo_info "df"
 df -Ph | head -n 1
-df -Ph | grep -E "mapper|sd" | sort
+df -Ph | grep_disk
 
 echo_info "/proc/mounts"
-cat /proc/mounts | grep -E "mapper|sd"
+cat /proc/mounts | grep_disk
