793cbee4bde1b7dce271d2c4595cee13a170c747
root <root@750e3ccc7a25>
2019-05-02 13:19:15 +0000

    add backend

--- python/celery/tasks.py
+++ python/celery/tasks.py
@@ -1,8 +1,12 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//') # backend: optional
+
+import time
 
 @app.task
-def add(x, y):
+def add(x, y, sleep_time=1):
+    print(f"sleep {sleep_time}s") # no output, use logging instead
+    time.sleep(sleep_time)
     return x + y
 
