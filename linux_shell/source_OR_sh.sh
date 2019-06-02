9186d8008bd34c0c879139c6697f9b237282aada
fzinfz <fzinfz@gmail.com>
2019-06-02 21:35:39 +0800

    update source_OR_sh

--- linux_shell/source_OR_sh.sh
+++ linux_shell/source_OR_sh.sh
@@ -1,21 +1,24 @@
-# sh/source working; bash not working
+shopt -s expand_aliases
+
+echo "\$0: [$0]"
 
 # write in one line: not working
-if [ "$0" = "source_OR_bash.sh" ]; then
-	echo "called by $"
-	alias q=exit; 
+if [ "$0" = "source_OR_sh.sh" ]; then
+	echo "called by *sh*"
+	alias q=exit
 else 
 	echo "called by source"
-	alias q=return; 
+	alias q=return
 fi
 
-echo '$0:'$0
-
+echo
 echo 'ps of $$:'
 ps -p $$ -o comm=
 
+echo
 echo 'ps tree of $$:'
 ps -efHww | grep $$ --color
 
+echo
 type q
-q
+q 11
