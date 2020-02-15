a04a3315112a37fc2b15425ee1cb75e9349226b9
Ferro Fang <fzinfz@gmail.com>
2020-02-15 21:44:10 +0800

    update 1st line for init.sh

--- linux/check_git.sh
+++ linux/check_git.sh
@@ -1,4 +1,4 @@
-[ -f init.sh ] && source init.sh || source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/fzinfz/scripts/master/linux/init.sh)"
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"; [ -f $SCRIPTPATH/init.sh ] && source $SCRIPTPATH/init.sh || source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/fzinfz/scripts/master/linux/init.sh)"
 
 p=$1
 for d in $(ls -d $p/*); do
--- linux/check_video.sh
+++ linux/check_video.sh
@@ -1,4 +1,4 @@
-[ -f init.sh ] && source init.sh || source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/fzinfz/scripts/master/linux/init.sh)"
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"; [ -f $SCRIPTPATH/init.sh ] && source $SCRIPTPATH/init.sh || source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/fzinfz/scripts/master/linux/init.sh)"
 
 check_video_card() {
     run "cat_one_line_files /sys/class/drm/card$1/device/{label,uevent,power_method,power_dpm_state}"
