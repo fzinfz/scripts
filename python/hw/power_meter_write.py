bed27d4e6db0249ab30c45e7fb88f2fbfda49325
Ferro Fang <fzinfz@gmail.com>
2020-11-01 03:47:01 +0800

    draft

---
+++ python/hw/power_meter_write.py
@@ -0,0 +1,27 @@
+import time, sys, re
+
+# algorithms taken from: https://github.com/glx-technologies/meter-dlt645
+
+def write_meter(chn, addr, cmd, data):
+    payload = cmd + passwd + opid + data    
+    chn.encode(addr, 0x14, payload)
+    chn.xchg_data(verbose=1, retry=0)   
+
+    
+str_to_bcd = lambda text: [ int(s, 16) for s in re.findall('..', text) ]
+    
+    
+def change_date(chn, addr):
+    new_date = str_to_bcd(time.strftime('%y%m%d', time.localtime())) + [0x00]
+    cmd     = [0x01, 0x01, 0x00, 0x04]
+    write_meter( chn, addr, cmd, list(reversed(new_date)) )
+    
+    
+def change_time(chn, addr):
+    t0 = time.time() + 1 # add 1s for the delay time
+    new_time = str_to_bcd(time.strftime('%H%M%S', time.localtime(t0)))
+    cmd     = [0x02, 0x01, 0x00, 0x04]
+    write_meter( chn, addr, cmd, list(reversed(new_time)) )    
+
+    
+     
