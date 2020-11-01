d2a868429252f76be9768bb4c0727e94d503259b
Ferro Fang <fzinfz@gmail.com>
2020-11-01 20:19:03 +0800

    update flask demo

--- python/flask/print_request_headers.py
+++ python/flask/print_request_headers.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print(request.headers)
-    return "hello"
+    return "check console for headers output"
---
+++ python/flask/print_request_headers.sh
@@ -0,0 +1,3 @@
+export FLASK_ENV=development
+export FLASK_APP=print_request_headers.py
+flask run --host=0.0.0.0 --no-reload
