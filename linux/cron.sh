dd065a964b972a3ea189358770303094ca50bb4b
Ferro Fang <fzinfz@gmail.com>
2020-11-23 23:08:50 +0800

    fix & update

--- linux/cron.sh
+++ linux/cron.sh
@@ -7,7 +7,12 @@ mkdir -p /var/log/cron
 run "service cron status | head -3"
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"; 
-run "echo '5-55/10 * * * * root $SCRIPTPATH/git.sh $dir_git_parent status 2>&1 > /var/log/cron/git.log' | crontab -"
+cat <<EOF | crontab -
+SHELL=/bin/bash
+
+5-55/10 * * * * $SCRIPTPATH/git.sh $dir_git_parent pull &>/var/log/cron/git.log
+EOF
 
 run "crontab -l"
 echo_tip "To edit manually: crontab -e"
+echo_tip "To check log: tail -f /var/log/syslog | grep -i cron"
--- linux/git.sh
+++ linux/git.sh
@@ -3,10 +3,13 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"; [ -f $SCRIPTPAT
 [ -z "$1" ] && read -p "Parent folder: " p || p=$1
 [ -z "$2" ] && action='status' || action=$2
 
+echo_script_header
+
 for d in $(ls -d $p/*); do
     if [ -d $d/.git ]; then
-        ls -d $d
-        cd $d && git $action
+        cd $d && run "git $action # $d"
         echo_yellow $(linesep -)
     fi
 done
+
+echo_script_header
