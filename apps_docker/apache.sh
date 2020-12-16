d174279e30ff154baf0b271fd0af6c56aab06e90
Ferro Fang <fzinfz@gmail.com>
2020-12-16 01:25:23 -0500

    fix & update

--- apps_docker/apache.sh
+++ apps_docker/apache.sh
@@ -1,12 +1,16 @@
 . ../web/init.sh
 
+n=apache
+
+apache_setup(){
+
 [ -z "$DIR_CERT" ] && read -p "Dir of .crt/.key: " DIR_CERT && export DIR_CERT=$DIR_CERT
 PATH_CRT=${DIR_CERT}/${MY_DOMAIN}.cer
 PATH_KEY=${DIR_CERT}/${MY_DOMAIN}.key
 ls -l $PATH_CRT $PATH_KEY
 [ $? -ne 0 ] && echo_error "modify $0 and re-try" && exit
 
-n=apache ; run "docker stop $n 2>/dev/null; docker rm $n 2>/dev/null"
+run "docker stop $n 2>/dev/null; docker rm $n 2>/dev/null"
 
 run "docker run --name $n -d --restart unless-stopped --net host \
     -v apache:/usr/local/apache2/htdocs \
@@ -17,6 +21,20 @@ run "docker run --name $n -d --restart unless-stopped --net host \
     httpd \
 "
 
+}
+
+#################################
+
+docker ps | grep $n
+if [ $? -eq 0 ]; then
+    read -p "restart apache? (y/n) " a
+    [ "$a" = 'y' ] && docker exec -it apache apachectl -k restart
+    
+    read -p "Re-create container? (y/n) " a
+    [ "$a" = 'y' ] && apache_setup
+fi
+
 sleep 2
 run "netstat -lntup | grep httpd"
+run "ls /var/lib/docker/volumes/apache/*"
 run "docker logs -f $n"
--- apps_docker/jupyter.sh
+++ apps_docker/jupyter.sh
@@ -3,16 +3,17 @@ source ../linux/init.sh
 folder_to_map=/data
 
 n=jupyter
-docker ps | grep $n
-[ $? -eq 0 ] && run "docker logs $n 2>&1 | grep token"
 
-read -p "Re-create container? (y/n) " a
-[ "$a" != 'y' ] & exit
+docker ps | grep $n
+if [ $? -eq 0 ]; then
+    run "docker logs $n 2>&1 | grep token"
+    read -p "Re-create container? (y/n) " a
+    [ "$a" != 'y' ] && exit
+fi
 
 docker stop $n 2>/dev/null; docker rm $n 2>/dev/null
 
-cmd="
-docker run --name $n \
+cmd="docker run --name $n \
     --net host \
     -v $folder_to_map:$folder_to_map -w $folder_to_map \
     -d --restart unless-stopped \
@@ -33,6 +34,3 @@ cmd="$cmd fzinfz/jupyter \
 run "$cmd"
 
 run "docker logs $n -f"
-
-
-
