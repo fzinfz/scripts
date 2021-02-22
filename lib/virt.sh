7401a93b5c27e4074dedc9843034a307c6c8e22e
fzinfz <fzinfz@gmail.com>
2021-02-22 20:18:34 +0800

    update lib/virt.sh

--- lib/virt.sh
+++ lib/virt.sh
@@ -1,15 +1,20 @@
-. ./ovs.sh
-
 virt_install(){
 
-[ -z "$1" ] && read -p 'image file: ' file_img || file_img=$1
+files=( $(ls *.qcow2 *.img | sort) )
+for i in "${!files[@]}"; do
+  printf '[%s] %s\n' "$i" "${files[i]}"
+done
+
+read -p "which to run: " i
+[[ $i =~ [0-9]+ ]] || exit_err non-num
+file_img=${files[i]}
 
 s="\
 virt-install --name $(echo $file_img | perl -pe "s/\.\w+$//") \
 --ram 512 --vcpu 2 \
 --disk path=$(pwd)/$file_img,device=disk,bus=ide "
 
-for ovsbr in $(ovs_br); do
+for ovsbr in $(ovs-vsctl list-br); do
     s="$s --network bridge=$ovsbr,model=virtio,virtualport_type=openvswitch"
 done
 
