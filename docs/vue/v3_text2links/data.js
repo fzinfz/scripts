4b847e291aace2d3b083bc8f02bae548fcb662f3
Ferro <fzinfz@gmail.com>
2022-10-05 03:39:14 +0800

    update text2links

--- docs/vue/v3_text2links/data.js
+++ docs/vue/v3_text2links/data.js
@@ -1,10 +1,16 @@
 const s = `
 
-Links |
-Source | https://github.com/fzinfz/scripts/tree/master/docs/vue
-Github | https://github.com/fzinfz
+Homepage - Text to Link |
+Source | https://github.com/fzinfz/scripts/tree/master/docs/vue/v3_text2links
+Data  | /data.js
 
-Tools |
-Excel to Markdown | https://html.ferro.pro/md.html
+== Similar Projects Below == | More Beautiful and Powerful
+sui: json to links | https://github.com/jeroenpardon/sui
+flame: sui + built-in GUI editors | https://github.com/pawelmalak/flame
+flare: flame + faster | https://github.com/soulteary/docker-flare
+dashy: flame + features | https://github.com/lissy93/dashy#status-indicators-
+reorx/sui2: sui + keyboard | https://github.com/reorx/sui2
 
+My other tools |
+Excel to Markdown | http://html.ferro.pro/md.html
 `;
