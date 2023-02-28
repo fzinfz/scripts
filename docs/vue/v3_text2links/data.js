53159039cd5d6fc695facc8fbdd0ad9ef24e13b8
Ferro <fzinfz@gmail.com>
2023-02-28 18:49:36 +0800

    tmp update

--- docs/vue/v3_text2links/data.js
+++ docs/vue/v3_text2links/data.js
@@ -1,16 +1,20 @@
 const s = `
 
-Homepage - Text to Link |
-Source | https://github.com/fzinfz/scripts/tree/master/docs/vue/v3_text2links
-Data  | /data.js
+Vue3 - Text to Full Links |
+Source🍃lines<100 | https://github.com/fzinfz/scripts/blob/master/docs/vue/v3_text2links/index.html
+Test relative link | ./data.js
+Test non-link | 简体繁體、にほんご、한국어、ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω
 
-== Similar Projects Below == | More Beautiful and Powerful
+Similar Projects - More Powerful |
 sui: json to links | https://github.com/jeroenpardon/sui
 flame: sui + built-in GUI editors | https://github.com/pawelmalak/flame
 flare: flame + faster | https://github.com/soulteary/docker-flare
 dashy: flame + features | https://github.com/lissy93/dashy#status-indicators-
 reorx/sui2: sui + keyboard | https://github.com/reorx/sui2
+PHP + SQLite 3 | https://github.com/helloxz/onenav
+支持Chrome插件 | https://github.com/luchenqun/my-bookmark
+support Firefox/Chrome extension | https://github.com/go-shiori/shiori
 
-My other tools |
+My other web tools & Test non-https links |
 Excel to Markdown | http://html.ferro.pro/md.html
 `;
